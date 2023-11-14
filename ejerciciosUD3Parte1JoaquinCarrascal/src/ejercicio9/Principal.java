a17f567d9c935dca201e039835ba5467017e4973
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2023-11-14 13:35:12 +0100

    subida ej9 vacio

---
+++ ejerciciosUD3Parte1JoaquinCarrascal/src/ejercicio9/Principal.java
@@ -0,0 +1,23 @@
+package ejercicio9;
+
+public class Principal {
+	
+	/*9. Crear un programa que gestione un sorteo de lotería. La lotería de Navidad se realiza sobre números que
+	van desde el 00001 al 99999 (no tendremos en cuenta series). Para facilitar, tendremos solo la clase Sorteo
+	y la principal para probar todo. El programa debe hacer lo siguiente:
+	• Comprar un décimo (eligiendo el número por teclado).
+	• Comprar un décimo generado por la máquina (aleatorio).
+	• Hacer sorteo (generará aleatoriamente el número premiado).
+	• Comprobar si nuestro décimo está premiado devolviendo un boolean.
+	• Mostrar si eres ganador o no.
+	Nota: si alguien quiere "entrenar" más, se pueden realizar todos los ejercicios hechos hasta ahora en los temas 1 y 2 mediante
+	programación orientada a objetos.*/
+
+	public static void main(String[] args) 
+	{
+		
+		
+
+	}
+
+}
