882e4443000c477f068c12a284cef42df9836fd3
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2023-11-21 13:33:05 +0100

    menores cambios

--- ejerciciosUD3Parte1JoaquinCarrascal/src/ejercicio9/Sorteo.java
+++ ejerciciosUD3Parte1JoaquinCarrascal/src/ejercicio9/Sorteo.java
@@ -1,8 +1,21 @@
 package ejercicio9;
 
+import java.util.Random;
+
 public class Sorteo 
 {
 
+	private int numPremiado;
 	
-	
+	public Sorteo(int numPremiado) {
+		this.numPremiado=numPremiado;
+	}
+
+	public int getNumPremiado() {
+		return numPremiado;
+	}
+
+	public void setNumPremiado(int numPremiado) {
+		this.numPremiado = numPremiado;
+	}
 }
