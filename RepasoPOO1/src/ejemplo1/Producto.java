5afd884b6c9ce095128b500af21b1d93d37d4251
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2023-12-12 13:33:38 +0100

    +rep

---
+++ RepasoPOO1/src/ejemplo1/Producto.java
@@ -0,0 +1,18 @@
+package ejemplo1;
+
+public class Producto {
+
+	private String nombreProd;
+	private double precFabr;
+	private int seccion;
+	private boolean enVenta;
+	
+	public Producto(String nombreProd, double precFabr, int seccion, boolean enVenta) {
+		this.nombreProd = nombreProd;
+		this.precFabr = precFabr;
+		this.seccion = seccion;
+		this.enVenta = enVenta;
+	}
+	
+	
+}
---
+++ RepasoPOO1/src/ejemplo1/ppal.java
@@ -0,0 +1,17 @@
+package ejemplo1;
+
+public class ppal {
+
+	public static void main(String[] args) 
+	{
+
+		//Producto p = new Producto ();//mal!
+		
+		Producto p1 = new Producto ("Lechuga",1.20,1,true);
+		//generar getters y setters.
+		
+		
+		
+	}
+
+}
