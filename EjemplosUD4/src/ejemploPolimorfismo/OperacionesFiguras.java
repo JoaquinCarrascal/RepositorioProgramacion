26329df833bc93fcd54500c76199e46f931f5b89
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2024-01-16 13:33:36 +0100

    fix importante

--- EjemplosUD4/src/ejemploPolimorfismo/OperacionesFiguras.java
+++ EjemplosUD4/src/ejemploPolimorfismo/OperacionesFiguras.java
@@ -5,5 +5,24 @@ public class OperacionesFiguras {
 	public double calcularAreaFigura(Figura f) {
 		return f.calcularArea();
 	}
-
+	
+	public double calcularListaCompleta(Figura[]listado) {
+		double resultado = 0;
+		for (int i = 0; i < listado.length; i++) {
+			resultado += calcularAreaFigura(listado[i]);
+		}
+		return resultado;
+	}
+	
+	public double calcularConAviso(Figura [] listado) {
+		double resultado = 0;
+		for (int i = 0; i < listado.length; i++) {
+			resultado += calcularAreaFigura(listado[i]);
+			if(listado[i] instanceof Circulo)
+				((Circulo)listado[i]).mostrarRadianes();
+		}
+		return resultado;
+		
+	}
+	
 }
--- EjemplosUD4/src/ejemploPolimorfismo/Principal.java
+++ EjemplosUD4/src/ejemploPolimorfismo/Principal.java
@@ -4,8 +4,24 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
+		OperacionesFiguras op1 = new OperacionesFiguras();
+		int tam = 5;
+		Figura [] lista= new Figura[tam]; //los array de clases abstractas se pueden instanciar
+		
 		Cuadrado cu1= new Cuadrado("Primer cuadrado", "Rojo", 1.0);
 		Circulo ci1= new Circulo ("Primer Círculo", "Azul", 1.0);
+		Cuadrado cu2= new Cuadrado("cuadrado", "Rojo", 1.0);
+		Circulo ci2= new Circulo ("Círculo", "Azul", 1.0);
+		Cuadrado cu3= new Cuadrado("cuadrado", "Rojo", 1.0);
+		
+		lista[0] = cu1;
+		lista[1] = ci1;
+		lista[2] = cu2;
+		lista[3] = ci2;
+		lista[4] = cu3;
+		
+		System.out.println(op1.calcularListaCompleta(lista));
+		
 		System.out.println(cu1);
 		System.out.println(ci1);
 		System.out.println("*********Área y perímetro**********");
