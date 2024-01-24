5f93dbc5b70e4c475f0b72db1830d07807b3d2f0
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2024-01-24 13:59:34 +0100

    24/01

--- ejerciciosInterfacesPOO2/src/ejercicio1/GestionPapeleria.java
+++ ejerciciosInterfacesPOO2/src/ejercicio1/GestionPapeleria.java
@@ -1,36 +1,37 @@
 package ejercicio1;
 
-public class GestionPapeleria implements ITriggerLibros{
+public class GestionPapeleria implements ITriggerLibros {
 
 	@Override
-	public int contadorPrestados(Object[] o) {
-		int contador = 0;
-		for (int i = 0; i < o.length; i++) {
-			if(o[i] instanceof Libro || o[i] instanceof Revista) {
-				if(o[i] instanceof Libro) {
-					if(((Libro)o[i]).isPrestado())
-						contador++;
-				}
-				else {
-					if(((Revista)o[i]).isPrestado())
-						contador++;
-				}
-			}
-		}
-		return contador;
-	}
-
-	@Override
-	public int publicacionesAnterioresA(Papeleria [] p, int anyo) {
+	public int contadorPrestados(Papeleria[] p) {
 		int contador = 0;
 		for (int i = 0; i < p.length; i++) {
-			if(p[i].getAnyoPublicacion() < anyo) {
-				contador++;
+			if (p[i] != null) {
+				if (p[i] instanceof Libro || p[i] instanceof Revista) {
+					if (p[i] instanceof Libro) {
+						if (((Libro) p[i]).isPrestado())
+							contador++;
+					} else {
+						if (((Revista) p[i]).isPrestado())
+							contador++;
+					}
+				}
 			}
 		}
 		return contador;
 	}
-	
-	
-	
+
+	@Override
+	public int publicacionesAnterioresA(Papeleria[] p, int anyo) {
+		int contador = 0;
+		for (int i = 0; i < p.length; i++) {
+			if (p[i] != null) {
+				if (p[i].getAnyoPublicacion() < anyo) {
+					contador++;
+				}
+			}
+		}
+		return contador;
+	}
+
 }
--- ejerciciosInterfacesPOO2/src/ejercicio1/ITriggerLibros.java
+++ ejerciciosInterfacesPOO2/src/ejercicio1/ITriggerLibros.java
@@ -2,7 +2,7 @@ package ejercicio1;
 
 public interface ITriggerLibros {
 
-	int contadorPrestados(Object [] o);
+	int contadorPrestados(Papeleria [] p);
     int publicacionesAnterioresA(Papeleria [] p, int anyo);
 	
 }
