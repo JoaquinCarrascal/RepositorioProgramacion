ed1ee020321d72d42a93aa5f63ad03cec235bbbc
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2024-02-13 13:47:15 +0100

    mistico

--- ejemplosUD5/src/ejemploSet/Persona.java
+++ ejemplosUD5/src/ejemploSet/Persona.java
@@ -1,5 +1,7 @@
 package ejemploSet;
 
+import java.util.Objects;
+
 public class Persona {
 
 	private String nombre;
@@ -39,6 +41,23 @@ public class Persona {
 	@Override
 	public String toString() {
 		return "Persona [nombre=" + nombre + ", dni=" + dni + ", edad=" + edad + "]\n";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dni, edad, nombre);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return Objects.equals(dni, other.dni) && edad == other.edad && Objects.equals(nombre, other.nombre);
 	}
 	
 	
---
+++ ejemplosUD5/src/sobreescribiendoEquals/Principal.java
@@ -0,0 +1,11 @@
+package sobreescribiendoEquals;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
