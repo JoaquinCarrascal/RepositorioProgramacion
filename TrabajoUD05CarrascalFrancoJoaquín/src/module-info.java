5182b12ed61ed7a5d5eb47f7392e3a9da65ac1c9
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2024-02-14 09:29:58 +0100

    proyecto ud 5

---
+++ TrabajoUD05CarrascalFrancoJoaquín/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module TrabajoUD05CarrascalFrancoJoaquín {
+}
---
+++ TrabajoUD05CarrascalFrancoJoaquín/src/trabajo_investigacion/Principal.java
@@ -0,0 +1,39 @@
+package trabajo_investigacion;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Principal {
+	public static void main(String[] args) {
+		
+		var texto = "Hola mundo"; //el compilador entiende que esto es un String
+		var texto2 = 23;//entiende que esto es un int
+		var texto3 = 10;
+		var texto4 = 4L; //con esta L le hemos dejado una pista de que este 4 es un Long
+		var auxiliar = 0.0;
+		//var auxiliar; //la variable ha de estar inicializada de lo contrario no puede funcionar
+		
+		Set <Persona> listadoTest = new HashSet<Persona>();
+		
+		List <Persona> listado = new ArrayList</*no hace falta poner Persona aqui*/>();
+		
+		//var saludo = "hola";
+		//saludo = 123L;  //no es posible
+		
+		
+		//aqui un pequeño test
+		System.out.println(texto);
+		
+		auxiliar = texto2 + texto3;
+		System.out.println(texto2 + " + " + texto3 + " = " + auxiliar);
+		
+		System.out.println(texto4);
+		System.out.println(listado);
+		System.out.println(listadoTest);
+		
+	}
+	
+}
+
